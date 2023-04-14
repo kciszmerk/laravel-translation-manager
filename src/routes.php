f3528523a4f934173503af349fbdccc8fa0774a2
kciszmerk <108678253+kciszmerk@users.noreply.github.com>
2023-04-14 11:26:30 +0200

    Update routes.php

--- src/routes.php
+++ src/routes.php
@@ -3,18 +3,18 @@
 declare(strict_types=1);
 
 $config = array_merge(config('translation-manager.route'), ['namespace' => 'Barryvdh\TranslationManager']);
-Route::group($config, function($router)
+Route::group($config, function($router) use ($config)
 {
-    $router->get('view/{groupKey?}', 'Controller@getView')->where('groupKey', '.*');
-    $router->get('/{groupKey?}', 'Controller@getIndex')->where('groupKey', '.*');
-    $router->post('/add/{groupKey}', 'Controller@postAdd')->where('groupKey', '.*');
-    $router->post('/edit/{groupKey}', 'Controller@postEdit')->where('groupKey', '.*');
-    $router->post('/groups/add', 'Controller@postAddGroup');
-    $router->post('/delete/{groupKey}/{translationKey}', 'Controller@postDelete')->where('groupKey', '.*');
-    $router->post('/import', 'Controller@postImport');
-    $router->post('/find', 'Controller@postFind');
-    $router->post('/locales/add', 'Controller@postAddLocale');
-    $router->post('/locales/remove', 'Controller@postRemoveLocale');
-    $router->post('/publish/{groupKey}', 'Controller@postPublish')->where('groupKey', '.*');
-    $router->post('/translate-missing', 'Controller@postTranslateMissing');
+    $router->get('view/{groupKey?}', 'Controller@getView')->name($config['as'].'.view')->where('groupKey', '.*');
+    $router->get('/{groupKey?}', 'Controller@getIndex')->name($config['as'].'.groupKey')->where('groupKey', '.*');
+    $router->post('/add/{groupKey}', 'Controller@postAdd')->name($config['as'].'.add')->where('groupKey', '.*');
+    $router->post('/edit/{groupKey}', 'Controller@postEdit')->name($config['as'].'.edit')->where('groupKey', '.*');
+    $router->post('/groups/add', 'Controller@postAddGroup')->name($config['as'].'.groups');
+    $router->post('/delete/{groupKey}/{translationKey}', 'Controller@postDelete')->name($config['as'].'.delete')->where('groupKey', '.*');
+    $router->post('/import', 'Controller@postImport')->name($config['as'].'.import');
+    $router->post('/find', 'Controller@postFind')->name($config['as'].'.find');
+    $router->post('/locales/add', 'Controller@postAddLocale')->name($config['as'].'.locales-add');
+    $router->post('/locales/remove', 'Controller@postRemoveLocale')->name($config['as'].'.locales-remove');
+    $router->post('/publish/{groupKey}', 'Controller@postPublish')->where('groupKey', '.*')->name($config['as'].'.publish');
+    $router->post('/translate-missing', 'Controller@postTranslateMissing')->name($config['as'].'.translate-missing');
 });
